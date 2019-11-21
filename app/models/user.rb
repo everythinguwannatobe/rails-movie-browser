e3268ceb7ec94aa2b4b5211f2dfcb67ddee5544e
spencerduncanpower <timdonerpower@gmail.com>
2019-11-21 15:11:54 -0500

    Add user data validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
+  has_many :watch_list
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\z/i.freeze
+
+  validates :email, presence: true, format: EMAIL_REGEX
+  validates :password, length: { in: 6..20 }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+
+  end
 end

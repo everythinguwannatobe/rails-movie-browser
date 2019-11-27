7e9010ff866b1cc05b71c4506cc98ea1e6766b44
spencerduncanpower <timdonerpower@gmail.com>
2019-11-27 12:47:44 -0500

    Upgrade packages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  # get 'movies/index'
-  # get 'movies/edit'
+
   get 'watch_lists/index'
   get 'watch_lists/show'
   get 'watch_lists/edit'
   get 'watch_lists/delete'
   # devise_for :users
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   resources :movies
+
   root 'movies#index'
 
 end

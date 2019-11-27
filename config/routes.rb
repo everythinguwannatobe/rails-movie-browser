521f73ee8ca5f3897bf8cab16bba9b4c74465fc8
spencerduncanpower <timdonerpower@gmail.com>
2019-11-27 12:40:07 -0500

    # If applied, this commit will...
    # Why is this change needed?
    Migrate test methods to movie controller
    1. MOve popular method 
    2. Destroy welcome resource originally used as a static route. 
    # How does it address the issue?
    
    
    # Provide links to any relevant tickets, articles or other

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :movies
-  root 'welcome#index'
+  root 'movies#index'
 
 end

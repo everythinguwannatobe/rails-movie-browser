af55eafb07c3914b587c3f160f9e36888cbd5e28
spencerduncanpower <timdonerpower@gmail.com>
2019-12-06 16:20:51 -0500

    # If applied, this commit will...
    # Why is this change needed?
    
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other resources
    Modify database schema

--- db/migrate/20191121161320_create_watch_lists.rb
+++ db/migrate/20191121161320_create_watch_lists.rb
@@ -3,6 +3,8 @@ class CreateWatchLists < ActiveRecord::Migration[6.0]
     create_table :watch_lists do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :movie, null: false, foreign_key: true
+      t.text :movie_title
+      t.text :poster_path
       t.text :comments
 
       t.timestamps null: false

624b75f85983b62dfbe858e3745cfafa877a5306
spencerduncanpower <timdonerpower@gmail.com>
2019-12-06 16:22:11 -0500

    # If applied, this commit will...
    # Why is this change needed?
    
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other resources
    Clean up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
 
-  #get 'users/show'
+  # get 'users/show'
   # devise_for :users
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  # Redirect login to root_path
+  namespace :users do
+    redirect to: 'movie#index'
+  end
+
+  root 'movies#index'
 
   resources :users, only: %i[show index] do
     resources :watch_lists
   end
+  resources :watch_lists
 
   resources :movies
-
-  root 'movies#index'
 
 end

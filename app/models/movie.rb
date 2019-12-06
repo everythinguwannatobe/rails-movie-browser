d448acfe5bc3590772257682753ef9a5724398b5
spencerduncanpower <timdonerpower@gmail.com>
2019-12-06 16:23:09 -0500

    # If applied, this commit will...
    # Why is this change needed?
    
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other resources
    Add nested attributes to the model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   has_many :watch_lists
   has_many :user, through: :watch_lists
 
+  accepts_nested_attributes_for :watch_lists
+
+  # default_scope { order(title: :asc) }
+
 end
--- app/models/watch_list.rb
+++ app/models/watch_list.rb
@@ -1,4 +1,6 @@
 class WatchList < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  accepts_nested_attributes_for :movie
 end

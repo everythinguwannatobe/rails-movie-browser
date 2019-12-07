fa4eacac2a96c7ba571df548a1461275650a71d8
spencerduncanpower <timdonerpower@gmail.com>
2019-12-07 11:25:32 -0500

    # If applied, this commit will...
    # Why is this change needed?
    
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other resources
    Fix bug in database save:
    1. Remove @movie['movie_id']
    2. Replace key with @movie['id']
    3, Fix destroy method to find_by_movie_id

--- app/controllers/watch_lists_controller.rb
+++ app/controllers/watch_lists_controller.rb
@@ -15,38 +15,28 @@ class WatchListsController < ApplicationController
   end
 
   def edit
-    # binding.pry
     @movie = Movie.find(params[:id])
     @watch_list = WatchList.find_by_movie_id(params[:id])
     if @watch_list.nil?
-      @watch_list = WatchList.create do |m|
+      @watch_list = WatchList.new do |m|
         m.user_id = current_user['id']
-        m.movie_id = @movie['movie_id']
+        m.movie_id = @movie['id']
         m.movie_title = @movie['title']
         m.poster_path = @movie['poster_path']
       end
       @watch_list.save!
-      #redirect_to user_watch_lists_path(current_user)
-    else
-      # redirect_path user_watch_lists_path(current_user)
     end
-    # binding.pry
-    # @watch_list = WatchList.find_by_movie_id(params[:movie_id])
-    # binding.pry
   end
 
   def update
     @watch_list = WatchList.find(params[:id])
-    @watch_list.update(watch_list_params)
-    if @watch_list.save
-      watch_list_path(@watch_list)
-    else
-      redirect_to user_watch_lists_path(current_user)
+    if @watch_list.update(watch_list_params)
+      redirect_to user_watch_lists_path(current_user), alert: 'You movie has been saved.'
     end
   end
 
   def destroy
-    @watch_list = WatchList.find(params[:id])
+    @watch_list = WatchList.find_by_movie_id(params[:id])
     @watch_list.destroy
     redirect_to watch_lists_path
   end
